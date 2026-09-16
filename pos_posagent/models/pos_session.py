2c9c91eb3142b36bf623b6eb8da043fa108b1fff
Abdelrhman Ashraf <124148126+abdelrahmanashraf-code@users.noreply.github.com>
2026-09-16 17:56:26 +0300

    Load preparation routes in POS

---
+++ pos_posagent/models/pos_session.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models
+
+
+class PosSession(models.Model):
+    _inherit = "pos.session"
+
+    @api.model
+    def _load_pos_data_models(self, config):
+        models_to_load = super()._load_pos_data_models(config)
+        if "posagent.preparation.route" not in models_to_load:
+            models_to_load += ["posagent.preparation.route"]
+        return models_to_load
